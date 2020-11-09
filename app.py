96bc4c66abc005f0fe7ebace7309cc7dec2386bf
Kelley Lynch <kelleylynch@gmail.com>
2020-11-09 13:16:23 -0500

    initial commit, currently using hardcoded manifest

---
+++ app.py
@@ -0,0 +1,82 @@
+import json
+import os
+import requests
+import tempfile
+
+from clams import Mmif
+from clams.vocab import MediaTypes, AnnotationTypes
+from lapps.discriminators import Uri
+from flask import Flask, request, render_template, flash, redirect, send_from_directory
+from werkzeug.utils import secure_filename
+
+from iiif_utils import *
+
+app = Flask(__name__, static_url_path="")
+
+
+@app.route('/uv/<path:path>')
+def send_js(path):
+    return send_from_directory("uv", path)
+
+
+@app.route('/temp/<path:path>')
+def send_temp(path):
+    return send_from_directory("temp", path)
+
+
+def display_iiif():
+    return render_template('player_page.html')
+
+
+@app.route('/display', methods=["GET"])
+def display_file():
+    mmif_str = requests.get(request.args["file"]).text
+    iiif_manifest = generate_iiif_manifest(mmif_str)
+    with open(os.path.join('temp', 'manifests', 'manifest.json'), 'w') as mani:
+        mani.write(iiif_manifest)
+    return display_iiif(mmif_str)
+
+
+def upload_display(filename):
+    f = open("temp/" + filename)
+    mmif_str = f.read()
+    f.close()
+    return display_iiif(mmif_str)
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join('temp', filename))
+            return upload_display(filename)
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form> 
+    '''
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    # TODO (krim @ 10/1/19): parameterize port number
+    app.run(port=5000, debug=True)
---
+++ iiif_utils.py
@@ -0,0 +1,80 @@
+def generate_iiif_manifest(mmif_str):
+    # generate mmif_obj from string
+    # get all views with timeframe annotations from mmif obj
+    # for all timeframe annotations
+    # generate a iiif thing
+    return '''{
+    "id": "http://dlib.indiana.edu/iiif_av/lunchroom_manners",
+    "type": "Manifest",
+    "label": "Sample Newshour",
+    "description": "A sample Newshour Video",
+    "sequences": [
+        {
+            "id": "http://dlib.indiana.edu/iiif_av/lunchroom_manners/sequence/1",
+            "type": "Sequence",
+            "canvases": [
+                {
+                    "id": "http://dlib.indiana.edu/iiif_av/lunchroom_manners/canvas/1",
+                    "type": "Canvas",
+                    "label": "A sample Newshour Video",
+                    "description": "a sample newshour video from the aapb",
+                    "height": 360, 
+                    "width": 480, 
+                    "duration": 660,
+                    "content": [
+                        {
+                            "id": "...",
+                            "type": "AnnotationPage",
+                            "items": [
+                                {
+                                    "id": "...",
+                                    "type": "Annotation",
+                                    "motivation": "painting",
+                                    "body": [
+                                        {
+                                            "type": "Choice",
+                                            "choiceHint": "user",
+                                            "items": [
+                                                {
+                                                    "id": "http://localhost:5000/static/cpb-aacip-507-0z70v8b343.mp4",
+                                                    "type": "Video",
+                                                    "label": "High",
+                                                    "format": "video/mp4"
+                                                }
+                                            ]
+                                        }
+                                    ],
+                                    "target": "http://dlib.indiana.edu/iiif_av/canvas/1"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    "structures": [
+        {
+            "id": "http://dlib.indiana.edu/iiif_av/lunchroom_manners/range/3",
+            "type": "Range",
+            "label": "SMPTE Bars",
+            "members": [
+                {
+                    "id": "http://dlib.indiana.edu/iiif_av/lunchroom_manners/canvas/1#t=0,59",
+                    "type": "Canvas"
+                }
+            ]
+        },
+        {
+            "id": "http://dlib.indiana.edu/iiif_av/lunchroom_manners/range/3",
+            "type": "Range",
+            "label": "Slate",
+            "members": [
+                {
+                    "id": "http://dlib.indiana.edu/iiif_av/lunchroom_manners/canvas/1#t=60,112",
+                    "type": "Canvas"
+                }
+            ]
+        }
+    ]
+}'''
