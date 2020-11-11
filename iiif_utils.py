7cfa74f3142c22dfb5c7cbb952891c3a26f65822
Kelley Lynch <kelleylynch@gmail.com>
2020-11-11 10:15:31 -0500

    adding timeframe annotation to iiif

--- iiif_utils.py
+++ iiif_utils.py
@@ -22,6 +22,7 @@ def generate_iiif_manifest(mmif_str):
                 "id": f"mmif_example_manifest.json/sequence/1",
                 "type": "Sequence",
                 "canvases": [],
+                "structures":[]
             }
         ]
     }
@@ -67,13 +68,34 @@ def generate_iiif_manifest(mmif_str):
             f"static{document_path}",
         )
         iiif_json["sequences"][0]["canvases"].append(canvas)
+        break # todo currently only supports single document, needs more work to align canvas values
 
     # # get all views with timeframe annotations from mmif obj
     tf_views = mmif_obj.get_all_views_contain(AnnotationTypes.TimeFrame.value)
 
-    for view in tf_views:
-        print (view)
-        pass
+    for id, view in enumerate(tf_views):
+        if view.properties["unit"] != "frame":
+            continue
+        start_fn = view.properties["start"]
+        end_fn = view.properties["end"]
+        frame_rate = 29.97
+        frame_type = view.properties.get("frameType")
+
+        start_sec = start_fn/frame_rate
+        end_sec = end_fn/frame_rate
+
+        structure = {
+            "id":f"mmif_example_manifest.json/range/{id}",
+            "type":"Range",
+            "label":f"{frame_type}",
+            "members":[
+                {
+                    "id":f"mmif_example_manifest.json/canvas/{1}/t={start_sec},{end_sec}", # need to align id here to support more than one document
+                    "type":"Canvas"
+                }
+            ]
+        }
+        iiif_json["structures"].append(structure)
 
     # # generate a iiif manifest and save output file
     with open(os.path.join("temp", "manifests", "manifest.json"), "w") as out:
